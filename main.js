92411f41d05c1ef5a0ef88ec72bb5ac05fa040e7
Guilherme <guilherme.h.rocha@outlook.com>
2023-05-29 14:21:54 -0300

    Add CSS of the PlayerName component

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
   "beforeend",
   `
-    ${PlayerName("Player 1")}
-    ${PlayerName("Player 2")}
+    ${PlayerName("Player1")}
+    ${PlayerName("Player2")}
     ${BoardGame(6)}
   `
 );
