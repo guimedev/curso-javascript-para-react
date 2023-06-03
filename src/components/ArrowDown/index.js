4bcbd69a878feb96cb1cdd5b2fb553a716a97a50
Guilherme <guilherme.h.rocha@outlook.com>
2023-06-03 12:11:19 -0300

    Add CSS in ArrowDown component with animation

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,11 @@
-function ArrowDown() {
+import "./style.css";
+
+function ArrowDown(currentPlayer = 1) {
   return /*html*/ `
-    <img src="images/IconArrowDown.png" alt="ícone de seta apontada para baixo">
+    <img class="arrow-down"
+         data-currentPlayer="${currentPlayer}"
+         src="images/IconArrowDown.png" 
+         alt="ícone de seta apontada para baixo">
   `;
 }
 
